69cfc36c572920c0ab62adedb78eaa1a73ad9386
Administrator <administrator@B280.local>
2017-05-22 10:50:41 +0200

    Do stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+## dependencies
+node
+grunt
+
+
+## helplinks
+http://markdown.de/
+https://www.railstutorial.org/book/beginning#sec-version_control
+http://ryanchristiani.com/getting-started-with-grunt-and-sass/
--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,21 @@
-/**
- * Created by administrator on 22.05.17.
- */
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'css/style.css' : 'scss/style.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+}
